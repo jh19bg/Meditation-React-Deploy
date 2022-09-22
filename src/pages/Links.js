befbe158b2d304470eb13cd3dccf5f8c1e2a54a3
Jon Hair 😺🛠 <johair@microsoft.com>
2022-09-22 01:08:41 -0400

    correct grammar and other errors

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -13,7 +13,7 @@ export const Links = () => (
                     <br></br>
                     <br></br>
 
-                    <li><i><a href='https://www.nccih.nih.gov/health/meditation-and-mindfulness-what-you-need-to-know' target='_blank'>National Center for Complementary and Integrative Health</a></i></li>
+                    <li><i><a href='https://www.nccih.nih.gov/health/meditation-and-mindfulness-what-you-need-to-know' target='_blank'>National Center for Complementary and Integrative Health - Meditation and Mindfulness: What You Need To Know</a></i></li>
                     <li><i><a href='https://www.mindful.org/how-to-meditate/' target='_blank'>Mindful - How to Meditate</a></i></li>
                     <li><i><a href='https://www.psychologytoday.com/us/basics/meditation' target='_blank'>Psychology Today - Meditation</a></i></li>
                 
@@ -25,7 +25,7 @@ export const Links = () => (
                     <br></br>
 
                     <li><i><a href='https://communitymeditation.net/' target='_blank'>Community Meditation</a></i></li>
-                    <li><i><a href='https://www.mindfulleader.org/meditate-together' target='_blank'>Mindful Leader - Meditate Together</a></i></li>
+                    <li><i><a href='https://www.mindfulleader.org/meditate-together' target='_blank'>Meditate Together</a></i></li>
                     <li><i><a href='https://meditationchapel.org/' target='_blank'>Meditation Chapel</a></i></li>
 
                     <br></br>
